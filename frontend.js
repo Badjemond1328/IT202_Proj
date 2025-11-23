78808d1f2340aad8e9978149c5a723b09d62df2c
Adam Nguyen <aln22@njit.edu>
2025-11-23 13:51:37 -0500

    finish cart logic, all functional

--- frontend.js
+++ frontend.js
@@ -1,11 +1,49 @@
 function shoppingCartClick(){
-    let productName = document.querySelector(".productName").textContent;
+    let productName = document.querySelector("#productName").textContent;
     let productPrice = document.querySelector(".price").textContent;
-    let productImg = document.querySelector(".prod-img").getAttribute("src");
+    let productImg = document.querySelector("#productImg").getAttribute("src");
+    let quantity = 1;
 
-    console.log(productImg);
+    console.log(productName, productPrice, productImg);
 
-    console.log(productName, productPrice);
+    
+
+    (async () => {
+				try {
+					const response = await fetch("http://localhost:5500/api/cart");
+				
+					
+					const userCartData = await response.json();
+					console.log("Cart data:", userCartData);
+
+					
+					
+				} catch (err) {
+					console.error("Fetch failed:", err);
+				}
+			})();
+
+
+    try {
+        const responseProdInfo = fetch("http://localhost:5500/api/productInfoCart", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({productName, productPrice, productImg,quantity})
+        });
+    } catch (error) {
+        console.log(error);
+        alert(error);
+    }
+}
+
+function display(card){
+    
+
+    let productName = card.querySelector(".productName").textContent;
+    let productPrice = card.querySelector(".price").textContent;
+    let productImg = card.querySelector(".prod-img").getAttribute("src");
+
+    
 
     try {
         const responseProdInfo = fetch("http://localhost:5500/api/productInfo", {
@@ -17,4 +55,6 @@ function shoppingCartClick(){
         console.log(error);
         alert(error);
     }
+
+    
 }
--- shopping-cart/shoppingCart.js
+++ shopping-cart/shoppingCart.js
@@ -1,53 +1,122 @@
-function addProductToCart(productName, productPrice, productImg) {
+(async () => {
+    try {
+        const response = await fetch("http://localhost:5500/api/cart");
+    
+        
+        const userCartData = await response.json();
+        console.log("Cart data:", userCartData);
+        
+        addProductToCart(userCartData);
+        
+        
+    } catch (err) {
+        console.error("Fetch failed:", err);
+    }
+
+
+    try {
+        const response = await fetch("http://localhost:5500/api/getSubtotal");
+
+        
+        const priceValues = await response.json();
+        const {subtotal, tax, total} = priceValues;
+        console.log(subtotal, tax, total);
+        
+
+        document.getElementById("subtotal").textContent = "$" + subtotal;
+        document.getElementById("tax").textContent = "$" + tax;
+        document.getElementById("total").textContent = "$" + total;
+        
+        
+        
+    } catch (err) {
+        console.error("Fetch failed:", err);
+    }
+
+
+})();
+
+function addProductToCart(cart) {
     const itemsHolder = document.querySelector('.items');
 
-    // Create a new product div
-    const productDiv = document.createElement('div');
-    productDiv.classList.add('product', 'mb-3'); 
+    cart.forEach(item => {
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product', 'mb-3'); 
 
     
-    productDiv.innerHTML = `
-        <div class="row">
-            <div class="col-md-3">
-                <img class="img-fluid mx-auto d-block image" width="100" height="100" src="${productImg}">
-            </div>
-            <div class="col-md-8">
-                <div class="info">
-                    <div class="row">
-                        <div class="col-md-5 product-name">
-                            <div class="product-name">
-                                <p class="cart-item-name">${productName}</p>
-                                <div class="product-info">
-                                    <div>Size: <span class="value"></span></div>
+        productDiv.innerHTML = `
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-fluid mx-auto d-block image" width="100" height="100" src="${item.productImg}">
+                </div>
+                <div class="col-md-8">
+                    <div class="info">
+                        <div class="row">
+                            <div class="col-md-5 product-name">
+                                <div class="product-name">
+                                    <p class="cart-item-name">${item.productName}</p>
+                                    <div class="product-info">
+                                        <div><span class="value"></span></div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-4 quantity">
-                            <label for="quantity">Quantity:</label>
-                            <input type="number" value="1" class="form-control quantity-input">
-                        </div>
-                        <div class="col-md-3 price">
-                            <span>${productPrice}</span>
+                            <div class="col-md-4 quantity">
+                                <label for="quantity">Quantity:</label>
+                                <input type="number" value="${item.quantity}" class="form-control quantity-input">
+                            </div>
+                            <div class="col-md-3 price">
+                                <span>${item.productPrice}</span>
+                            </div>
                         </div>
                     </div>
+                    <div class="col-md-1 d-flex align-items-center">
+                    <button class="btn btn-danger btn-sm remove-item">Remove</button>
                 </div>
-                <div class="col-md-1 d-flex align-items-center">
-                <button class="btn btn-danger btn-sm remove-item">Remove</button>
+                </div>
+                
             </div>
             </div>
+        `;
+        
+
+        itemsHolder.appendChild(productDiv);
+
+        const qtyInput = productDiv.querySelector(".quantity-input");
+        qtyInput.addEventListener("change", async (e) => {
             
-        </div>
-        </div>
-    `;
-    
 
-    itemsHolder.appendChild(productDiv);
+            try {
+                const res = await fetch("http://localhost:5500/api/changeQty", {
+                    method: "POST", 
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({productName: item.productName , quantity: e.target.value }) 
+                });
 
-    productDiv.querySelector('.remove-item').addEventListener('click', () => {
-        productDiv.remove();
-        
-        
+                
+            } catch (err) {
+                console.error(err);
+            }
+            
+        });
+
+        productDiv.querySelector('.remove-item').addEventListener('click', async () => {
+            productDiv.remove();
+            
+             try {
+                const res = await fetch("http://localhost:5500/api/removeFromCart", {
+                    method: "POST", 
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({ productName: item.productName }) 
+                });
+
+                
+            } catch (err) {
+                console.error(err);
+            }
+            
+        });
     });
+    
     
     
 }
